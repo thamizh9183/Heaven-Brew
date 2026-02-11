f5bc9eaa40010aba740d516e2687e93e945907ba
thamizh9183 <kavithamil956@gmail.com>
2026-02-11 15:43:08 +0530

    Connected deployed backend

--- src/CoffeeShop/api/axios.js
+++ src/CoffeeShop/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://heaven-brew-backend.onrender.com/api/coffees"
+
 });
 
 // 🔐 attach token
